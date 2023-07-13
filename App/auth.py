056950d08657aaf209f4022fddc736c8b5b7bca7
Wmugacha <wilfredmugacha@gmail.com>
2023-07-13 08:50:27 +0300

    fixed record adding error

--- App/auth.py
+++ App/auth.py
@@ -22,7 +22,7 @@ def login():
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
-            flash('Email does not exist.', category='error')
+            flash('Username does not exist.', category='error')
 
     return render_template("login.html", user=current_user)
 
@@ -32,8 +32,8 @@ def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
-@auth.route('/Signup', methods=['GET', 'POST'])
-def Signup():
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
     if request.method == 'POST':
         email = request.form.get('email')
         username = request.form.get('username')
@@ -43,10 +43,8 @@ def Signup():
         user = User.query.filter_by(email=email).first()
         if user:
             flash('Email already exists', category='error')
-        elif len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
-        elif len(username) < 2:
-            flash('Username must be greater than 1 character.', category='error')
+        elif len(username) < 4:
+            flash('Username must be greater than 3 character.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
@@ -55,8 +53,8 @@ def Signup():
             new_user = User(email=email, username=username, password=generate_password_hash(password1, method='scrypt'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for('views.dashboard'))
+            return redirect(url_for('auth.login'))
 
     return render_template("signup.html", user=current_user)
--- App/views.py
+++ App/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Crop, Field, Harvest
+from datetime import datetime
 from . import db
 import json
 views = Blueprint('views', __name__)
@@ -11,4 +12,28 @@ def home():
 
 @views.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
-    return render_template("dashboard.html")
+    if request.method == 'POST':
+        field = request.form.get('field')
+        crop = request.form.get('crop')
+        harvest = request.form.get('harvest')
+        date = request.form.get('date')
+
+        user_id =current_user.id
+
+       # Create a new entry in the Crop table associated
+        new_entry = Crop(name=crop, user_id=user_id)
+        db.session.add(new_entry)
+        db.session.commit()
+        
+        # Create a new entry in the Field table associated with the crop
+        new_field = Field(name=field, crop=new_entry)
+        db.session.add(new_field)
+        db.session.commit()
+        
+        # Create a new entry in the Harvest table associated with the field
+        new_harvest = Harvest(crop_yield=harvest, date=datetime.strptime(date, '%Y-%m-%d'), field=new_field)
+        db.session.add(new_harvest)
+        db.session.commit()
+
+    crops = Crop.query.filter_by(user_id=current_user.id).all()
+    return render_template('dashboard.html', crops=crops)
